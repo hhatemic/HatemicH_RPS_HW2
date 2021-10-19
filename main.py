ad5e1265131365a8f5fecb09c8b9e173c2922e80
hhatemic <hamzahatemic12@gmail.com>
2021-10-19 12:43:27 -0400

    working on RPS

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Hamza"
+eyeColor = "hazel"
+heightInFeet = 6
+hairColour = "black"
+
+print("============================")
+print("My name is " + name + " and I have " + eyeColor + " eyes.")
+print("I also still have " + str(heightInFeet) + " feet tall and have")
+print("I think I look just fine!")
+print("============================")
+
